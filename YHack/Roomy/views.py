55de7a686ca02bea8e0f6a900cb3ce7cf52afa8f
David Zbarsky <dzbarsky@gmail.com>
2013-11-08 21:41:54 -0500

    add redirection to charge page

--- YHack/Roomy/views.py
+++ YHack/Roomy/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from Roomy.models import User
+
 def index(request):
     return render(request, 'Roomy/index.html')
 
@@ -10,3 +12,11 @@ def newUser(request):
     phone = request.POST['phone']
     user = User(name=name, email=email, phone=phone)
     user.save()
+
+def charge(request):
+    users = User.objects.all()
+    usersDict = []
+    for user in users:
+        usersDict.append(user)
+    return render(request, 'Roomy/charge.html', {'users': users})
+
