09f638ac82d3eca0de8c4b6c4ff4651d3f8564da
David Zbarsky <dzbarsky@gmail.com>
2013-11-09 04:13:12 -0500

    Cleanup various things, fix login page

--- YHack/Roomy/views.py
+++ YHack/Roomy/views.py
@@ -6,9 +6,10 @@ import json
 
 def signin(request):
     name = request.GET['name']
+    request.session['name'] = name
     existing = User.objects.filter(name=name)
     if existing.count() > 0:
-	user = User.objects.get(name=name)
+        user = User.objects.get(name=name)
         return render(request, 'Roomy/index.html', dict(getParams(), **{'roomyUser': user.id}))
     else:
         return createUser(request)
@@ -35,12 +36,12 @@ def createUser(request):
     return render(request, 'Roomy/newuser.html', getParams())
 
 def newUser(request):
-    name = request.POST['name']
+    name = request.session['name']
     email = request.POST['email']
     phone = request.POST['phone']
     user = User(name=name, email=email, phone=phone)
     user.save()
-    return HttpResponse()
+    return createHouse(request)
 
 def chores(request):
     return render(request, 'Roomy/chores.html', getParams())
@@ -48,9 +49,10 @@ def chores(request):
 def addChore(request):
     name = request.POST['name']
     frequency = request.POST['frequency']
+    day = request.POST['day']
     users = request.POST['users']
     users = [user for user in User.objects.all() if str(user.id) in users]
-    chore = Chore(name=name, frequency=frequency)
+    chore = Chore(name=name, frequency=frequency, day=day)
     chore.save()
     for user in users:
       chore.users.add(user)
@@ -86,7 +88,6 @@ def doCharge(request):
     charge.save()
     for user in users:
       charge.users.add(user)
-    print "SAVED"
     return HttpResponse()
 
 def lists(request):
